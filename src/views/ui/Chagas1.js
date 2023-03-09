e89dc92029e56f4f9b411bf8361ee32e5a9e451d
Vinicius Machado <viniciusmachado@Mac-mini-de-Vinicius-UFPI.local>
2023-03-09 16:45:11 -0300

    comit inicial

---
+++ src/views/ui/Chagas1.js
@@ -0,0 +1,60 @@
+import {
+  Card,
+  Row,
+  Col,
+  CardTitle,
+  CardBody,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+} from "reactstrap";
+
+const Chagas1 = () => {
+  return (
+    <div>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <script src="../files/actions.js"></script>
+      <script defer="defer" src="https://barra.brasil.gov.br/barra_2.0.js?ver=5.2.2" type="text/javascript"></script>
+      
+      <article id="upa" class="container">
+        
+        <div class="row">
+            <div class="col">
+                <h1 class="main-title">Pacientes com sinais e sintomas sugestivos de Doença de Chagas<a href="#" id="" class="" target="_self" title="Pacientes com sinais e sintomas sugestivos de Calazar"><i class="fas fa-info-circle align-top"></i></a></h1>
+            </div> 
+        </div>
+
+        <section class="d-none d-block d-sm-none d-md-block d-lg-block d-xl-block mg-bm">
+            <div class="row">
+                <div class="col">
+                    <h3 class="target green"><i class="fas fa-map-marker-alt lg"></i>Você esta em: <strong> Unidade de Atenção Primária</strong></h3>
+                    <div class="d-flex justify-content-center mg-tp">
+                        <div class="item green colorcards">
+                            <a href="#">
+                                <div class="content">
+                                    <i class="fas fa-map-marker-alt "></i>
+                                    <p class=""><strong>Atenção Secundária</strong> Confirmação diagnóstica de DCA</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </section>    
+
+      </article>
+      
+      chagas
+    </div>
+
+    
+
+  );
+};
+
+export default Chagas1;
